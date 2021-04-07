32ff1c5a70fcbce9ba4333e26c0fa5454cd08bd1
leeoocca <leeoocca@gmail.com>
2021-04-07 02:31:10 +0200

    begin to head in list_push_front

--- ft_list_push_front_bonus.s
+++ ft_list_push_front_bonus.s
@@ -2,8 +2,8 @@
 	global	_ft_list_push_front
 	extern	_malloc
 
-_ft_list_push_front:		; rdi = t_list **begin, rsi = void *data
-	push	rdi				; save begin
+_ft_list_push_front:		; rdi = t_list **head, rsi = void *data
+	push	rdi				; save head
 	push	rsi				; save data
 	mov		rdi, 16			; sizeof(t_list)
 	xor		rax, rax		; clear rax
@@ -13,8 +13,8 @@ _ft_list_push_front:		; rdi = t_list **begin, rsi = void *data
 	cmp		rax, 0
 	jz		return			; malloc failed
 	mov		[rax], rsi		; lst.data = rsi
-	mov		rcx, [rdi]		; tmp = *begin
+	mov		rcx, [rdi]		; tmp = *head
 	mov		[rax + 8], rcx	; lst.next = tmp
-	mov		[rdi], rax		; *begin = new
+	mov		[rdi], rax		; *head = new
 return:
 	ret
