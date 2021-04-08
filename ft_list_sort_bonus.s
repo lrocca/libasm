0256dd5ce0c5f9bd6da418d2f584ae480689d257
Leonardo Rocca <lrocca@student.42roma.it>
2021-04-08 19:07:59 +0200

    fix: ft_list_sort
    registers were modified when other function was called

--- ft_list_sort_bonus.s
+++ ft_list_sort_bonus.s
@@ -4,36 +4,41 @@
 ; void		ft_list_sort(t_list **rdi, int (*rsi)());
 
 _ft_list_sort:
+	push	r12
+	push	r13
+	push	r15
 	cmp		rdi, 0			; 1st arg == 0, return
 	jz		return
-	mov		r8, [rdi]		; r8 = *head
-	mov		rcx, rsi		; rcx = cmp function
+	mov		r12, [rdi]		; r12 = *head
+	mov		r15, rsi		; r15 = cmp function
 
 _loop:
-	mov		r9, [r8 + 8]	; r9 = lst.next
-	cmp		r9, 0			; lst.next == 0
+	mov		r13, [r12 + 8]	; r13 = lst.next
+	cmp		r13, 0			; lst.next == 0
 	jz		return			; lst is over
 	push	rdi				; save lst head
-	mov		rdi, [r8]		; 1st arg = lst.data
-	mov		rsi, [r9]		; 2nd arg = lst.next.data
-	call	rcx				; call compare function
+	mov		rdi, [r12]		; 1st arg = lst.data
+	mov		rsi, [r13]		; 2nd arg = lst.next.data
+	call	r15				; call compare function
 	pop		rdi				; restore lst head
 	cmp		rax, 0			; check return of cmp
 	jg		swap			; ret > 0, then swap
 	jmp		next			; else loop
 
 swap:
-	mov		r10, [r8]		; r10 = lst.data
-	mov		r11, [r9]		; r11 = lst.next.data
-	mov		[r8], r11		; lst.data = r11
-	mov		[r9], r10		; lst.next.data = r10
-	mov		r8, [rdi]		; reset lst head
-	; ret
+	mov		r10, [r12]		; r10 = lst.data
+	mov		r11, [r13]		; r11 = lst.next.data
+	mov		[r12], r11		; lst.data = r11
+	mov		[r13], r10		; lst.next.data = r10
+	mov		r12, [rdi]		; reset lst head
 	jmp		_loop
 
 next:
-	mov		r8, r9			; lst = lst.next
+	mov		r12, r13		; lst = lst.next
 	jmp		_loop
 
 return:
+	pop		r15
+	pop		r13
+	pop		r12
 	ret
